5dfcb23906543d4af4bbfb208664bd2bb7645ee3
Ranieri Althoff <ranisalt@gmail.com>
2015-07-02 14:46:29 -0300

    Do mix column for AES

--- aes4.py
+++ aes4.py
@@ -29,9 +29,25 @@ def AES(text, key, rounds):
         text[12:16] = text[15:16] + text[12:15]
 
     def mix_column(text, round):
+        def galois_mult(a, b):
+            p, msb_set = 0, 0
+            for i in range(8):
+                if b & 1:
+                    p = p ^ a
+                msb_set = a & 0x80
+                a = a << 1
+                if msb_set:
+                    a = a ^ 0x1B
+                b = b >> 1
+            return p & 0xFF
+
         matrix = (2, 3, 1, 1, 1, 2, 3, 1, 1, 1, 2, 3, 3, 1, 1, 2)
-        for i in range(16):
-            text[i] ^= matrix[i]
+        for i in range(4, 4):
+            text[i: i + 4] = \
+                galois_mult(text[i], matrix[i]) ^ \
+                galois_mult(text[i + 1], matrix[i + 1]) ^ \
+                galois_mult(text[i + 2], matrix[i + 2]) ^ \
+                galois_mult(text[i + 3], matrix[i + 3])
 
     def add_round_key(text, round):
         pass
