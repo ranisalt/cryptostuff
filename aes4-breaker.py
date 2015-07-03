e9f45218c17b33c7f24aa8e7258ce412a275b2f7
Ranieri Althoff <ranisalt@gmail.com>
2015-07-03 19:49:45 -0300

    what the fuck am I doing

--- aes4-breaker.py
+++ aes4-breaker.py
@@ -16,9 +16,21 @@ key = ''.join(random.sample(string.ascii_letters, 32))
 # cipher ingegrals with key
 integrals = gen_integral(1)
 ciphered = (aes4.AES128(x, key.encode(), 4) for x in integrals)
-pre_last_sr = (aes4.inv_shift_row(text) for text in ciphered)
+pre_last_sr = list(aes4.inv_shift_row(text) for text in ciphered)
+possible_key = []
+for _ in range(32):
+    possible_key.append([])
 
-xoration = []
-kick = 0
+for pos in range(16):
+    for kick in range(256):
+        # invert kick bits?
+        nkick = ~kick & 0xFF
+        xoration = 0
+        for text in pre_last_sr:
+            # xor with xorred byte and kick
+            xoration ^= aes4.sbox.index(text[pos] ^ nkick)
 
+        if xoration == 0:
+            possible_key[pos].append(kick)
 
+pass
